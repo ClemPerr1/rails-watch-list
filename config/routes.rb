55f96b4fc59f517e44fb63c5fd40b150f47fa0ca
Clement Perrin <113604057+ClemPerr1@users.noreply.github.com>
2023-01-07 16:12:38 +0100

    Update index view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "lists", to: "lists#index"
-  get "lists/new", to: "lists#new"
+  get "lists/new", to: "lists#new", as: :new
   post "lists", to: "lists#create"
   get "lists/:id", to: "lists#show"
 end
